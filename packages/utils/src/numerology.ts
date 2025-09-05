40f4d1cc6418f54c23a7d26e2223da89aed752c5
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-05 20:06:11 +0200

    fix: numerology algo

--- packages/utils/src/numerology.ts
+++ packages/utils/src/numerology.ts
@@ -6,59 +6,84 @@ const days = Array.from({ length: 31 }, (_, i) => i + 1); // 1-31
 const masterNumbers = [11, 22, 33];
 
 export const numerology = {
-    calculateLifePath: (day: string, month: string, year: string) => {
-        const [dayNumber, monthNumber, yearNumber] = [day, month, year].map(
-            (part) => parseInt(part),
-        );
+  calculateLifePath: (day: string, month: string, year: string) => {
+    const [dayNumber, monthNumber, yearNumber] = [day, month, year].map(
+      (part) => parseInt(part)
+    );
 
-        if (!dayNumber || !monthNumber || !yearNumber) {
-            throw new Error('Invalid date');
-        }
-
-        if (!days.includes(dayNumber)) {
-            throw new Error('Invalid day');
-        }
-        if (!monthNumbers.includes(monthNumber)) {
-            throw new Error('Invalid month');
-        }
-        if (!yearNumbers.includes(yearNumber)) {
-            throw new Error('Invalid year');
-        }
-        day = day.replace(/9/, '');
-        month = month.replace(/9/, '');
-        year = year.replace(/9/, '');
-
-        let lifePathNumber = dayNumber + monthNumber + yearNumber;
-        while (lifePathNumber > 9) {
-            if (masterNumbers.includes(lifePathNumber)) {
-                break;
-            }
-            lifePathNumber = lifePathNumber
-                .toString()
-                .split('')
-                .reduce((acc, curr) => acc + parseInt(curr), 0);
-        }
-        if (![...materialNumbers, ...masterNumbers].includes(lifePathNumber)) {
-            throw new Error('Invalid life path number');
-        }
-        return lifePathNumber;
-    },
-    calculateThePsychicNumber: (day: number) => {
-        if (!days.includes(day)) {
-            throw new Error('Invalid day');
-        }
-
-        let sum = day;
-        while (sum > 9) {
-            sum = sum
-                .toString()
-                .split('')
-                .reduce((acc, curr) => acc + parseInt(curr), 0);
-        }
-        return sum;
-    },
-
-    isMasterNumber: (number: number) => {
-        return masterNumbers.includes(number);
+    if (!dayNumber || !monthNumber || !yearNumber) {
+      throw new Error("Invalid date");
     }
+
+    if (!days.includes(dayNumber)) {
+      throw new Error("Invalid day");
+    }
+    if (!monthNumbers.includes(monthNumber)) {
+      throw new Error("Invalid month");
+    }
+    if (!yearNumbers.includes(yearNumber)) {
+      throw new Error("Invalid year");
+    }
+
+    let yearSum = year.split("").reduce((acc, curr) => acc + parseInt(curr), 0);
+    while (yearSum > 9 && !masterNumbers.includes(yearSum)) {
+      yearSum = yearSum
+        .toString()
+        .split("")
+        .reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
+
+    let monthSum = month
+      .split("")
+      .reduce((acc, curr) => acc + parseInt(curr), 0);
+    while (monthSum > 9 && !masterNumbers.includes(monthSum)) {
+      monthSum = monthSum
+        .toString()
+        .split("")
+        .reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
+
+    let daySum = day.split("").reduce((acc, curr) => acc + parseInt(curr), 0);
+    while (daySum > 9 && !masterNumbers.includes(daySum)) {
+      daySum = daySum
+        .toString()
+        .split("")
+        .reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
+    console.log({ yearSum, monthSum, daySum });
+
+    let lifePathNumber = daySum + monthSum + yearSum;
+    while (lifePathNumber > 9) {
+      if (masterNumbers.includes(lifePathNumber)) {
+        break;
+      }
+      console.log({ lifePathNumber });
+      lifePathNumber = lifePathNumber
+        .toString()
+        .split("")
+        .reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
+    if (![...materialNumbers, ...masterNumbers].includes(lifePathNumber)) {
+      throw new Error("Invalid life path number");
+    }
+    return lifePathNumber;
+  },
+  calculateThePsychicNumber: (day: number) => {
+    if (!days.includes(day)) {
+      throw new Error("Invalid day");
+    }
+
+    let sum = day;
+    while (sum > 9) {
+      sum = sum
+        .toString()
+        .split("")
+        .reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
+    return sum;
+  },
+
+  isMasterNumber: (number: number) => {
+    return masterNumbers.includes(number);
+  },
 };
