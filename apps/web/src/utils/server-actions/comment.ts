b789ae5179276cb6083729af998eea726ee9b3ba
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-09 09:22:33 +0200

    feat: revaldiated comments and replies after submission on the server

--- apps/web/src/utils/server-actions/comment.ts
+++ apps/web/src/utils/server-actions/comment.ts
@@ -1,10 +1,12 @@
 'use server';
 
 import { ICelebrityCommentWrite, nosqlDB } from '@workspace/db/nosql';
+import { revalidatePath } from 'next/cache';
 
 export const commentServerAction = async (comment: ICelebrityCommentWrite) => {
     'use server';
     await nosqlDB.models.CelebrityComment.createTopLevel({
         ...comment,
     });
+    revalidatePath(`/celebrity/${comment.celebrityId}`);
 };
--- apps/web/src/utils/server-actions/reply.ts
+++ apps/web/src/utils/server-actions/reply.ts
@@ -1,10 +1,12 @@
 'use server';
 
 import { ICelebrityReplyWrite, nosqlDB } from '@workspace/db/nosql';
+import { revalidatePath } from 'next/cache';
 
 export const replyServerAction = async (reply: ICelebrityReplyWrite) => {
     'use server';
     await nosqlDB.models.CelebrityComment.createReply({
         ...reply,
     });
+    revalidatePath(`/celebrity/${reply.celebrityId}`);
 };
