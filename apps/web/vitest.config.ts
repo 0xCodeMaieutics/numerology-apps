15ed145971605f99e064b6c92ef97d8873043279
0xCodeMaieutics <gio.shara12345@gmail.com>
2025-09-16 17:42:36 +0200

    revert(web): vitest

--- apps/web/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-    test: {
-        include: ['src/__test__/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    },
-});
